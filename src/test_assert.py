7de89498071482e70a7473450b85b2e77898158d
Yuka Otsuki <data2000analysis@gmail.com>
2024-04-18 20:39:07 +0900

    add: test file

---
+++ src/test_assert.py
@@ -0,0 +1,24 @@
+import sys
+
+sys.path.append("src")
+
+from src.main import main
+from src.generate import Trainer, GenerateText
+import warnings
+
+# DeprecationWarning を無視する
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+
+def test_main():
+    main(
+        fname="data/inputs/sample/disease_semple100.csv",
+        input_column="出現形",
+        predict_column="正規形",
+        reliability_column="正規形チェックフラグ",
+        rank="C",
+        batch_size=4,
+        epoch_num=1,
+        device="mps",
+    )
+    assert True
